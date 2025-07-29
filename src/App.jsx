0514393be8e9321209c97df8e79fa55dae9201bf
ash1198 <ash.ranjan09@gmail.com>
2025-07-29 13:13:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+            <div style={{ textAlign: 'center', marginTop: '100px' }}>
+                <h1>Hello World 🌍</h1>
+            </div>
+        </>
+    );
+}
+export default App;
